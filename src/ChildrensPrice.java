bf80da30c98fa9d5edb1c3a9ba2799aa7dc19daf
Mehmet Ali Incekara <xnappy@web.de>
2016-04-26 19:46:09 +0200

    fix compile problem

--- src/ChildrensPrice.java
+++ src/ChildrensPrice.java
@@ -7,11 +7,17 @@ public class ChildrensPrice extends Price {
         return Movie.CHILDRENS;
     }
 
+    @Override
     public double getCharge(int daysRented){
         double result = 1.5;
         if (daysRented > 3){
             result += (daysRented - 3) * 1.5;
         }
         return result;
+    }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
     }
 }
